f73ea1b411a9ba0ebe70d33f336f7bc9b41a5edf
JavGarCas <133601340+JavGarCas@users.noreply.github.com>
2023-06-13 11:56:03 -0600

    Use gradle 8 (#1)
    * Use gradle 8
    
    * Empty lines
    
    ---------
    
    Co-authored-by: JavGar <dushtajavier@hotmail.com>

---
+++ extensions-kt/src/main/java/com/javgarcas/extensions_kt/String.kt
@@ -0,0 +1,12 @@
+package com.javgarcas.extensions_kt
+
+import java.util.regex.Pattern
+
+fun String.Companion.empty() = ""
+
+fun String.Companion.space() = " "
+
+fun String?.safeString() = this ?: String.empty()
+
+fun String.isValidEmail(pattern: Pattern = Pattern.compile(EMAIL_PATTERN)) = pattern.matcher(this).matches()
+
